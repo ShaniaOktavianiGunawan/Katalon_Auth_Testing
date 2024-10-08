ba9aea2069b1c3adf8a71274d98af351fe1f4c16
ShaniaOktavianiGunawan <102290992+ShaniaOktavianiGunawan@users.noreply.github.com>
2024-10-08 21:14:31 +0700

    katalon-project

---
+++ Object Repository/Page_AHKAMERA APP/profil Icon.rs
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>profil Icon</name>
+   <tag></tag>
+   <elementGuidId>a1aab029-ad82-4985-8eb6-f2ae830bb764</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='headlessui-menu-button-:r1:']/img</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>img.h-8.w-8.p-1.rounded-full</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>img</value>
+      <webElementGuid>511b25aa-13ad-4391-9364-eb0b43028dfe</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>h-8 w-8 p-1 rounded-full</value>
+      <webElementGuid>3397a7aa-9469-4cee-b896-0996831763c9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>src</name>
+      <type>Main</type>
+      <value>/static/media/user.44ea40a3eb8dd7fed2a9.png</value>
+      <webElementGuid>6c39f41c-ba2b-4e25-ab44-9afa6a8ab0d3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;headlessui-menu-button-:r1:&quot;)/img[@class=&quot;h-8 w-8 p-1 rounded-full&quot;]</value>
+      <webElementGuid>6d16e7c4-154c-46f6-81dd-a97a473b7262</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//button[@id='headlessui-menu-button-:r1:']/img</value>
+      <webElementGuid>2e8d1740-fd31-41db-844d-fcb522cbf387</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button/img</value>
+      <webElementGuid>37d00bfc-dd15-4bb5-86ff-cdb5881f069e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//img[@src = '/static/media/user.44ea40a3eb8dd7fed2a9.png']</value>
+      <webElementGuid>fcada003-f8d1-4b90-b125-779a68d79137</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
